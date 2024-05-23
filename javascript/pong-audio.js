2ee534aaf0565b7afc0f012f06f88ad2050bc79b
MichaelLara2 <jarrydlara@gmail.com>
2024-05-23 12:15:35 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -55,9 +55,9 @@ soundArray.push(scoreSound);
 export var ambientSound = new soundFile("ambience.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -20; //turn down volume
+ambientSound.player.volume.value = -20; //turn up volume
 
-export var adventureMusic = new soundFile("ambience.mp3");
+export var adventureMusic = new soundFile("Spookywind.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
 adventureMusic.player.volume.value = -16;
